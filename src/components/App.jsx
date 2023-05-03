5e46b291aa4492877625477e5e78b26fb46220c3
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-03 15:06:10 +0300

    Adds Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Routes, Route, Link, NavLink, Form } from "react-router-dom";
+import { Home } from "pages/Home";
+import { Movies } from "pages/Movies";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
