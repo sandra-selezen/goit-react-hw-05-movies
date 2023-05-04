7956118d103e50bbb2492d6389b7b7af5fd38c00
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-04 19:12:31 +0300

    Adds get request for MovieDetails

--- src/services/fetchMovieDetails.js
+++ src/services/fetchMovieDetails.js
@@ -1,2 +1,18 @@
 import axios from "axios";
 import { API_KEY, URL } from "./constants";
+
+export const fetchMovieDetails = async (id) => {
+  const response = await axios.get(`${URL}movie/${id}?api_key=${API_KEY}`);
+
+  const { title, overview, poster_path, genres, vote_average } = response.data;
+  const movieGenres = genres.map(item => item.name).join(", ");
+  const movieInfo = {
+    title,
+    overview,
+    poster_path: `https://image.tmdb.org/t/p/w300${poster_path}`,
+    movieGenres,
+    vote_average: (vote_average * 10).toFixed(),
+  }
+
+  return movieInfo;
+}
