cdf6341d4fe676992c767941fc35c403a5441da8
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-05 16:09:09 +0300

    Update api functions

--- src/services/fetchMovieCast.js
+++ src/services/fetchMovieCast.js
@@ -4,5 +4,6 @@ import { API_KEY, URL } from "./constants";
 export const fetchMovieCast = async (id) => {
   const response = await axios.get(`${URL}movie/${id}/credits?api_key=${API_KEY}`);
   const { cast } = response.data;
+  
   return cast;
 }
--- src/services/fetchMovieReviews.js
+++ src/services/fetchMovieReviews.js
@@ -3,9 +3,7 @@ import { API_KEY, URL } from "./constants";
 
 export const fetchMovieReviews = async (id) => {
   const response = await axios.get(`${URL}movie/${id}/reviews?api_key=${API_KEY}`);
-
   const { results } = response.data;
-  console.log(results);
-
+  
   return results;
 }
