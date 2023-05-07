3903fc64b7726aab029fdfe56c24e7cf8dae69c0
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-07 17:16:57 +0300

    Adds styles for MovieDetails component

---
+++ src/components/MovieDetails/MovieDetails.styled.js
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const GoBackBtn = styled(Link)`
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 5px;
+  background: linear-gradient(90deg, hsl(277, 56%, 68%) 0%, hsl(203, 71%, 60%) 100%);
+  transition: box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  :hover {
+    box-shadow: 0 8px 48px hsla(203, 71%, 25%, .5);
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin: 24px auto;
+
+  @media screen and (max-width: 767px) {
+    width: 280px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 704px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 846px;
+  }
+`;
+
+export const DetailsList = styled.ul`
+  padding: 24px;
+  display: flex;
+  gap: 20px;
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const DetailsItem = styled.li`
+  @media screen and (min-width: 768px) {
+    width: calc(100% / 2);
+  }
+`;
+
+export const DetailsImg = styled.img`
+  width: 248px;
+  height: 315px;
+  border-radius: 5px;
+
+  @media screen and (min-width: 768px) {
+    width: 294px;
+    height: 400px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 375px;
+    height: 478px;
+  }
+`;
+
+export const MovieTitle = styled.h1`
+  margin-bottom: 20px;
+  font-weight: 700;
+  filter: drop-shadow(0 4px 4px #51AAFF);
+  
+  @media screen and (max-width: 767px) {
+    font-size: 1rem;
+  }
+`;
+
+export const MovieSubtitle = styled.h2`
+  margin-bottom: 12px;
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+export const MovieText = styled.p`
+  margin-bottom: 12px;
+`;
+
+export const InfoList = styled.ul`
+  display: flex;
+  gap: 20px;
+`;
+
+export const InfoLink = styled(Link)`
+  padding: 8px 12px;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 5px;
+  transition: background 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  :hover,
+  :focus {
+    background: linear-gradient(90deg, hsl(277, 56%, 68%) 0%, hsl(203, 71%, 60%) 100%);
+  }
+`;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef, Suspense } from "react";
-import { Link, Outlet, useParams, useLocation } from "react-router-dom";
+import { Outlet, useParams, useLocation } from "react-router-dom";
 import { fetchMovieDetails } from "services/fetchMovieDetails";
+import { DetailsImg, DetailsItem, DetailsList, GoBackBtn, MovieText, MovieSubtitle, MovieTitle, Wrapper, InfoList, InfoLink } from "components/MovieDetails/MovieDetails.styled";
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -24,19 +25,27 @@ const MovieDetails = () => {
   
   return (
     <>
-      <Link to={backLinkRef.current}>Go Back</Link>
-      <h1>{title}</h1>
-      <p>User Score: {vote_average}%</p>
-      <img src={poster_path} alt={title} />
-      <p><b>Overview</b></p>
-      <p>{overview}</p>
-      <p><b>Genres</b></p>
-      <p>{movieGenres}</p>
-      <p>Additional information</p>
-      <ul>
-        <li><Link to={"cast"}>Cast</Link></li>
-        <li><Link to={"reviews"}>Reviews</Link></li>
-      </ul>
+      <GoBackBtn to={backLinkRef.current}>Go Back</GoBackBtn>
+      <Wrapper>
+        <DetailsList>
+          <DetailsItem>
+            <DetailsImg src={poster_path} alt={title} />
+          </DetailsItem>
+          <DetailsItem>
+            <MovieTitle>{title}</MovieTitle>
+            <MovieText>User Score: {vote_average}%</MovieText>
+            <MovieSubtitle>Overview</MovieSubtitle>
+            <MovieText>{overview}</MovieText>
+            <MovieSubtitle>Genres</MovieSubtitle>
+            <MovieText>{movieGenres}</MovieText>
+            <MovieSubtitle>Additional information</MovieSubtitle>
+            <InfoList>
+              <li><InfoLink to={"cast"}>Cast</InfoLink></li>
+              <li><InfoLink to={"reviews"}>Reviews</InfoLink></li>
+            </InfoList>
+          </DetailsItem>
+        </DetailsList>
+      </Wrapper>
       <Suspense>
         <Outlet />
       </Suspense>
